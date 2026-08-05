cb0526313aceb1af3a78373594c2d864d1af61a7
TestAutomation714 <ganaiduhk@gmail.com>
2026-08-05 22:32:06 +0530

    Update DemoTest.java

--- src/com/test/DemoTest.java
+++ src/com/test/DemoTest.java
@@ -4,10 +4,10 @@ public class DemoTest {
 
 	public int sum()
 	{
-		int a=10, b=20,c;
+		int a=10, b=20,c,ad;
 		c=a+b;
 		//System.out.println(c);
-		return a;
+		return ad;
 	}
 	
 	public static void main(String[] args) {
